3c82e4b9ab27b26c7d67896be3b3731786cd92a5
Dexter Valencia <deeside@pacbell.net>
2020-02-13 10:14:26 -0800

    updated routes, globalState missing View Saved

--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -1,7 +1,31 @@
 import axios from "axios";
 
 export default {
-    getBooks: function(query) {
-        return axios.get("/api/books", { params: {q: query} });
-    }
-};
+    // Gets all books
+    getPosts: function() {
+      return axios.get("/api/books");
+    },
+    // Gets the books with the given id
+    getPost: function(id) {
+      return axios.get("/api/books/" + id);
+    },
+    // Deletes the books with the given id
+    deletePost: function(id) {
+      return axios.delete("/api/books/" + id);
+    },
+    // Saves a post to the book database
+    savePost: function(bookData) {
+      return axios.post("/api/books", bookData);
+    },
+    //  View Saved books 
+    getPost: function(bookData) {
+     return axios.post("/api/books/", bookData);
+    },     
+  };
+
+
+  // export default {
+//     getBooks: function(query) {
+//         return axios.get("/api/books", { params: {q: query} });
+//     }
+// };
